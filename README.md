755c19a30058db3c2c173a0b8422feba84091e4a
Kim Levin <kbussie1@montgomerycollege.edu>
2020-04-14 21:54:47 -0400

    my first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Program gets three numbers and prints out the smallest and largest number
---
+++ src/SmallAndLarge.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class SmallAndLarge {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input the 1st number: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Input the 2nd number: ");
+        int num2 = in.nextInt();
+
+        System.out.print("Input the 3rd number: ");
+        int num3 = in.nextInt();
+
+        if (num1 > num2) {
+            if (num1 > num3){
+                System.out.println("The largest: " + num1);
+                if (num2 < num3)
+                    System.out.println("The smallest: " + num2);
+            }
+            else if(num1 < num3){
+                System.out.println("The largest: " + num3);
+                System.out.println("The smallest: " + num2);
+            }
+        }
+        if (num2 > num3) {
+            if (num2 > num1){
+                System.out.println("The largest: " + num2);
+                if (num3 > num1)
+                    System.out.println("The smallest: " + num1);
+                else if (num3 < num1)
+                    System.out.println("The smallest: " + num3);
+            }
+            if (num2 < num1) {
+                System.out.println("The largest: " + num1);
+                System.out.println("The smallest: " + num3);
+            }
+        }
+        if (num3 > num1){
+            if (num3  > num2){
+                System.out.println("The largest: " + num3);
+                if (num1 > num2) {
+                    System.out.println("The smallest: " + num2);
+                } else if (num1 < num2)
+                    System.out.println("The smallest: " + num1);
+            }
+            if (num3  < num2){
+                System.out.println("The largest: " + num2);
+                System.out.println("The smallest: " + num1);
+            }
+        }
+    }
+}
